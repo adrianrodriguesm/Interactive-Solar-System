c8bba5cea650f4e2458bb86bd37ff09f7cae1c1f
Victor Ribeiro <victor.portusil0@gmail.com>
2020-01-17 13:52:56 +0000

    Comment partially commented.

--- Engine For Merging/Shaders/jupiterV.glsl
+++ Engine For Merging/Shaders/jupiterV.glsl
@@ -23,8 +23,12 @@ void main(void)
 {
 	gl_Position =  ProjectionMatrix*ViewMatrix * ModelMatrix * in_Position;
 	v_TexCoord = texCoord;
+    //Normals are calculated using the Normal matrix, in order to avoid the deformation of Normals if a scale matrix is applied previously
     v_Normals = mat3(transpose(inverse(ViewMatrix * ModelMatrix))) * normals;
+    //We calculate each frament position in View Space
     v_FragPos = ViewMatrix * ModelMatrix * in_Position;
+    //Light position is calculated in View Space
     v_lightPosition = ViewMatrix * lightPosition;
+     //We need the camera position for lighting calculations
     v_CameraPos = vec4(0.0f,0.0f,-cameraValue,1.0f);
 }
